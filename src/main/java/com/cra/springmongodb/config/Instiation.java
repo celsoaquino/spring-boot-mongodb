893ee44c7689fd3a8d91782ef3e37d70fe48f346
Celso Aquino <celso.aquino@ymail.com>
2020-02-05 00:15:00 -0300

    Criando entity Post com User aninhado

--- src/main/java/com/cra/springmongodb/config/Instiation.java
+++ src/main/java/com/cra/springmongodb/config/Instiation.java
@@ -1,12 +1,16 @@
 package com.cra.springmongodb.config;
 
+import com.cra.springmongodb.domain.Post;
 import com.cra.springmongodb.domain.User;
+import com.cra.springmongodb.repositories.PostRepository;
 import com.cra.springmongodb.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 @Configuration
 public class Instiation implements CommandLineRunner {
@@ -14,16 +18,26 @@ public class Instiation implements CommandLineRunner {
     @Autowired
     private UserRepository repository;
 
+    @Autowired
+    private PostRepository postRepository;
+
     @Override
     public void run(String... args) throws Exception {
 
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+
         repository.deleteAll();
+        postRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo", maria);
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acorfei feliz", maria);
 
         repository.saveAll(Arrays.asList(maria, alex, bob));
+        postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
---
+++ src/main/java/com/cra/springmongodb/repositories/PostRepository.java
@@ -0,0 +1,9 @@
+package com.cra.springmongodb.repositories;
+
+import com.cra.springmongodb.domain.Post;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+}
