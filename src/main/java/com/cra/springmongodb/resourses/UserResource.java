2478f58f470a471c8223b65b7f6c1c8c431b5154
Celso Aquino <celso.aquino@ymail.com>
2020-02-04 23:39:18 -0300

    Atualizar usuario com Put

--- src/main/java/com/cra/springmongodb/resourses/UserResource.java
+++ src/main/java/com/cra/springmongodb/resourses/UserResource.java
@@ -43,6 +43,14 @@ public class UserResource {
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable String id){
         service.delete(id);
+        return ResponseEntity.noContent().build() ;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Void> update(@RequestBody UserDTO userDTO, @PathVariable String id){
+        User user = service.fromDTO(userDTO);
+        user.setId(id);
+        user = service.update(user);
         return ResponseEntity.noContent().build();
     }
 }
--- src/main/java/com/cra/springmongodb/services/UserService.java
+++ src/main/java/com/cra/springmongodb/services/UserService.java
@@ -27,13 +27,24 @@ public class UserService {
 
     public User insert(User user){
         return userRepository.insert(user);
-
     }
 
     public void delete(String id){
         findById(id);
         userRepository.deleteById(id);
     }
+
+    public User update(User user){
+        User newUser = userRepository.findById(user.getId()).get();
+        updateData(newUser, user);
+        return userRepository.save(newUser);
+    }
+
+    private void updateData(User newUser, User user) {
+        newUser.setName(user.getName());
+        newUser.setEmail(user.getEmail());
+    }
+
 
     public User fromDTO(UserDTO userDTO){
         return new User(userDTO.getId(), userDTO.getName(), userDTO.getEmail());
