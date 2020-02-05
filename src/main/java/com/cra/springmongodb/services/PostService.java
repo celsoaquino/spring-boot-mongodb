5f7f28a432253c2ab7c4e9ac72f15d5716252a8e
Celso Aquino <celso.aquino@ymail.com>
2020-02-05 12:08:51 -0300

    Consulta com vários critérios

--- src/main/java/com/cra/springmongodb/services/PostService.java
+++ src/main/java/com/cra/springmongodb/services/PostService.java
@@ -6,9 +6,11 @@ import com.cra.springmongodb.dto.UserDTO;
 import com.cra.springmongodb.repositories.PostRepository;
 import com.cra.springmongodb.repositories.UserRepository;
 import com.cra.springmongodb.services.exception.ObjectNotFoundException;
+import javafx.geometry.Pos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,5 +28,10 @@ public class PostService {
 
     public List<Post> findByTitle(String text){
        return postRepository.searchTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return postRepository.fullSearch(text, minDate, maxDate);
     }
 }
